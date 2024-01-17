54c3305b2f1654f22920e9dab118ffe6addb0a62
Roy-Uttam <royuttam553@gmail.com>
2024-01-17 18:01:54 +0600

    assesment-card modified

--- src/app/components/assesment/commonComponents/accordion-card/accordion-card.component.ts
+++ src/app/components/assesment/commonComponents/accordion-card/accordion-card.component.ts
@@ -2,7 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-
+import { addIcons } from 'ionicons';
+import {pencil,trash} from 'ionicons/icons'
 @Component({
   selector: 'app-accordion-card',
   templateUrl: './accordion-card.component.html',
@@ -16,7 +17,9 @@ import { IonicModule } from '@ionic/angular';
 })
 export class AccordionCardComponent  implements OnInit {
 
-  constructor() { }
+  constructor() {
+    addIcons({pencil,trash})
+   }
 
   ngOnInit() {}
 

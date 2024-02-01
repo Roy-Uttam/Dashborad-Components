eae88b661e6921da8d84b91e0c70166c1002ac94
Roy-Uttam <royuttam553@gmail.com>
2024-02-01 11:14:40 +0600

    tabs switvhing done

--- src/app/pages/asset-registration/asset-registration.page.ts
+++ src/app/pages/asset-registration/asset-registration.page.ts
@@ -13,60 +13,7 @@ import { IonicModule, SegmentChangeEventDetail } from '@ionic/angular';
 export class AssetRegistrationPage implements OnInit {
   segment: string = 'custom1';
 
-  elementToAppend: HTMLElement;
-
-  constructor(private renderer: Renderer2, private el: ElementRef) {
-    this.elementToAppend = this.renderer.createElement('div');
-    this.renderer.addClass(this.elementToAppend, 'appended-content');
-    this.renderer.appendChild(
-      this.elementToAppend,
-      this.renderer.createText(this.segment)
-    );
-  }
-
-  segmentChanged(event: any) {
-
-    this.removeAppendedElement();
-
-    const segmentButton = this.el.nativeElement.querySelector(
-      `ion-segment-button[value="${event.detail.value}"]`
-    );
-    const targetElement = segmentButton.querySelector(`ion-segment-button div`);
-
-    this.renderer.appendChild(targetElement, this.elementToAppend);
-  }
-
-  private removeAppendedElement() {
-    if (this.elementToAppend.parentNode) {
-      this.renderer.removeChild(
-        this.elementToAppend.parentNode,
-        this.elementToAppend
-      );
-    }
-  }
-
-  // segmentChanged(event: any) {
-  //   // console.log('Selected segment:', this.segment);
-
-  //   const segmentButton = this.el.nativeElement.querySelector(
-  //     `ion-segment-button[value="${event.detail.value}"]`
-  //   );
-
-  //   const newDiv = this.renderer.createElement('div');
-
-  //   const targetElement = segmentButton.querySelector(`ion-segment-button div`);
-
-  //   this.renderer.setProperty(
-  //     newDiv,
-  //     'innerHTML',
-  //     '<p>This is dynamically appended HTML!</p>'
-  //   );
-
-  //   this.renderer.appendChild(targetElement, newDiv);
-
-  //   console.log(targetElement);
-
-  // }
+  constructor() {}
 
   ngOnInit() {}
 }
--- src/app/pages/test/test.page.ts
+++ src/app/pages/test/test.page.ts
@@ -8,13 +8,83 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: './test.page.html',
   styleUrls: ['./test.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule],
 })
 export class TestPage implements OnInit {
+  //   segment: string = 'custom1';
 
-  constructor() { }
+  //   elementToAppend: HTMLElement;
 
-  ngOnInit() {
-  }
+  //   constructor(private renderer: Renderer2, private el: ElementRef) {}
 
+  //   segmentChanged(event: any) {
+
+  //     this.elementToAppend = this.renderer.createElement('div');
+  //     this.renderer.addClass(this.elementToAppend, 'appended-content');
+  //     this.renderer.appendChild(
+  //       this.elementToAppend,
+  //       this.renderer.createText(event.detail.value)
+  //     );
+
+  //     const segmentButton = this.el.nativeElement.querySelector(
+  //       `ion-segment-button[value="${event.detail.value}"]`
+  //     );
+  //     const targetElement = segmentButton.querySelector(`ion-segment-button div`);
+
+  //     this.removeAppendedElement();
+  //     this.renderer.appendChild(targetElement, this.elementToAppend);
+
+  //     const activeClass = this.el.nativeElement.querySelector(
+  //       '.segment-button-checked'
+  //     );
+  //     console.log(activeClass);
+  //   }
+
+  //   private removeAppendedElement() {
+  //     if (this.elementToAppend.parentNode) {
+  //       this.renderer.removeChild(
+  //         this.elementToAppend.parentNode,
+  //         this.elementToAppend
+  //       );
+  //     }
+  //   }
+
+  // segmentChanged(event: any) {
+  //   // console.log('Selected segment:', this.segment);
+
+  //   const segmentButton = this.el.nativeElement.querySelector(
+  //     `ion-segment-button[value="${event.detail.value}"]`
+  //   );
+
+  //   const newDiv = this.renderer.createElement('div');
+
+  //   const targetElement = segmentButton.querySelector(`ion-segment-button div`);
+
+  //   this.renderer.setProperty(
+  //     newDiv,
+  //     'innerHTML',
+  //     '<p>This is dynamically appended HTML!</p>'
+  //   );
+
+  //   this.renderer.appendChild(targetElement, newDiv);
+
+  //   console.log(targetElement);
+
+  // }
+
+  // showhide2(event: any) {
+  //   var btn2 = document.getElementById('.title');
+  //   btn2?.classList?.add('active');
+
+  //   if (btn2) {
+  //     btn2?.classList?.add('active');
+  //   }
+  //   btn2?.classList?.remove('active');
+
+  //   console.log(event.detail);
+  //   console.log(btn2);
+
+  // }
+
+  ngOnInit() {}
 }
